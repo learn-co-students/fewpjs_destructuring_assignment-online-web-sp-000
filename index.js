337a8f7dbafd9a2837d84b85336be8af5a9e88db
decentralvision <36858744+decentralvision@users.noreply.github.com>
2020-08-20 10:45:44 -0700

    Done.

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ const [, , , , , indg, ] = colors
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
 const {name, color, song, job, partner} = muppet
 
+// change
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 
 const song_2 = k_muppet.k_album.the_muppet_movie.song_2
